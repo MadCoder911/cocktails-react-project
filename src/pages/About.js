502b0e8195bd4ad6dbafea4a8e02944158c11b48
Royce119 <madcoder911@gmail.com>
2023-02-04 23:44:12 +0200

    initail

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">About us</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas excepturi
+        optio minima vitae laudantium. Est sapiente iste aspernatur nisi error
+        exercitationem ipsam animi quam eligendi sint tempore atque quibusdam
+        repellendus dolorem deserunt quis corporis quasi, voluptatum facilis,
+        expedita autem consequuntur.
+      </p>
+    </section>
+  );
+};
+
+export default About;
